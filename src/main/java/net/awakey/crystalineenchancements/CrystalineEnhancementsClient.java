529bb29882d2c2e17b9e47961e9c5b463fe47e44
AwakeyV <tusharjyotibaruah1@gmail.com>
2024-11-28 17:23:24 +0530

    Initial commit

---
+++ src/main/java/net/awakey/crystalineenchancements/CrystalineEnhancementsClient.java
@@ -0,0 +1,10 @@
+package net.awakey.crystalineenchancements;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrystalineEnhancementsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/awakey/crystalineenchancements/CrystallineEnhancementsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.awakey.crystalineenchancements;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrystallineEnhancementsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
